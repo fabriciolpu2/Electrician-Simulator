89c508adb0823f1fceb183d2964cfa4c8e409edd
Fabricio Silva <fabriciolpu2@gmail.com>
2018-10-26 14:48:09 -0400

    mecanica fusivel

---
+++ Demo-Electrician-Simulator/Assets/Fusivel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fusivel : MonoBehaviour {
+
+    public GameObject mao;
+    public GameObject eixoGancho;
+    //public Quaternion rotacaoCamera;
+    //public GameObject gancho;
+    // Use this for initialization
+    public bool fixarFusivelNoGancho = true;
+   
+
+
+    void Start () {
+        //hinge = GetComponent<HingeJoint>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.rotation = mao.transform.rotation;        
+        //transform.localRotation = mao.transform.rotation;
+        //transform.Rotate(0, eixoGancho.transform.rotation.y, mao.transform.rotation.z);
+        //transform.SetParent(eixoGancho.transform);
+        if(fixarFusivelNoGancho == true)
+        {
+            FixarFusivelGancho();
+        } else
+        {
+            DesfixarFusivelGancho();
+        }
+        
+    }
+
+    void FixarFusivelGancho()
+    {
+        transform.position = eixoGancho.transform.position;
+    }
+
+    void DesfixarFusivelGancho()
+    {
+        
+        Debug.Log("Desfixando");
+    }
+}
---
+++ Demo-Electrician-Simulator/Assets/FusivelMecanica.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FusivelMecanica : MonoBehaviour {
+
+    public Fusivel fusivel;
+    public HingeJoint hinge;
+    public Rigidbody suporteFusivelBasePoste;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {        
+        // Disable the spring on the HingeJoint component.
+        hinge = GetComponent<HingeJoint>();
+        rb = GetComponent<Rigidbody>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SensorFusivel")
+        {
+            fusivel.fixarFusivelNoGancho = false;
+            Debug.Log("Chegou aqui no sensor");
+            //hinge.useSpring = false;
+            hinge.connectedBody = null;
+            if (hinge.connectedBody == null)
+            {
+                Debug.Log("Aqui no nulo");                
+                hinge.anchor = new Vector3(-0.09f, -0.22f, 0);
+                hinge.connectedBody = suporteFusivelBasePoste;
+                rb.useGravity = true;
+            }
+            
+        }
+    }
+}
